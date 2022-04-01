ae0247ed6101bd45b476aee3895681e9c874f2f0
Rotthin <rotthin_dev@protonmail.com>
2022-04-01 22:59:29 +0200

    fixed wrong height value for settings-background-cover

--- js/particle.js
+++ js/particle.js
@@ -1,5 +1,6 @@
 import { particles, jointsEnabled } from './backgroundParticles.js';
 const moveSpeed = 100;
+const maxDist = 50;
 export class Particle {
     constructor(x, y, dir_x, dir_y, radius, color) {
         this.color = new Map([
@@ -21,9 +22,7 @@ export class Particle {
                     const dx = p.x - this.x;
                     const dy = p.y - this.y;
                     const dist = Math.sqrt(dx * dx + dy * dy);
-                    const maxDist = 50;
                     if (dist <= maxDist) {
-                        console.log(dist / maxDist);
                         ctx.beginPath();
                         ctx.lineWidth = 0.3;
                         ctx.moveTo(this.x, this.y);
