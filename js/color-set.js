a70bdb34de791aaa104c16cf0ac652fe9072a6bb
Rotthin <rotthin_dev@protonmail.com>
2022-04-03 02:01:04 +0200

    Removed js from git

--- js/color-set.js
+++
@@ -1,7 +0,0 @@
-export class ColorSet {
-    constructor(mainColor, secondaryColor, interactiveColor) {
-        this.mainColor = mainColor;
-        this.secondaryColor = secondaryColor;
-        this.interactiveColor = interactiveColor;
-    }
-}
--- js/mouse.js
+++
@@ -1,8 +0,0 @@
-export class Mouse {
-    constructor(radius) {
-        this.x = 0;
-        this.y = 0;
-        this.radius = radius;
-        this.active = false;
-    }
-}
